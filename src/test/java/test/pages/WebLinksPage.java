aff379b3e2306ce3de069e6282b25986bb66a7ec
tamanhtp <tamanhtp@gmail.com>
2019-12-23 16:50:59 +0700

    Add more 3 TestCase

--- src/test/java/test/pages/WebLinksPage.java
+++ src/test/java/test/pages/WebLinksPage.java
@@ -11,7 +11,9 @@ public class WebLinksPage extends GeneralPage{
 	private static By leftmenu_WebLinks_Categories = By.xpath("//ul[@id='submenu']//a[contains(text(),'Categories')]");
 	private static By leftmenu_WebLinks_Fields = By.xpath("//ul[@id='submenu']//a[contains(text(),'Fields')]");
 	private static By leftmenu_WebLinks_FieldGroup = By.xpath("//ul[@id='submenu']//a[contains(text(),'Field Groups')]");
-	
+	private By _divStatus_Weblinks = By.xpath("//select[@id='filter_published']/../div");
+	private String _liStatus = "//div[contains(@id,'filter_published')]//li[text()='%s']";
+	private By _btnSearchTools = By.xpath("//button[normalize-space(text())='Search Tools']");
 	// create new weblinks with title, alias, url, content, status
 	
 
@@ -31,8 +33,14 @@ public class WebLinksPage extends GeneralPage{
 	public static void gotoWeblinksFieldGroups() {
 		BrowserHelper.driver.findElement(leftmenu_WebLinks_FieldGroup).click();
 	}
-	
-	
+	public void selectStatus(String status){
+		BrowserHelper.driver.findElement(_divStatus_Weblinks).click();
+		BrowserHelper.driver.findElement(By.xpath(String.format(_liStatus, status))).click();
+	}
+	public void clickBtnSearchTools() throws InterruptedException{
+		BrowserHelper.driver.findElement(_btnSearchTools).click();
+		Thread.sleep(1000);
+	}
 	
 	
 }
---
+++ src/test/java/testNG/TC_JOOMLA_WEBLINKS_004.java
@@ -0,0 +1,62 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+import test.pages.GeneralPage;
+import test.pages.HomePage;
+import test.pages.LoginPage;
+import test.pages.WebLinksNewPage;
+import test.pages.WebLinksPage;
+import test.utils.Utilities;
+import test.utils.Constants;
+import test.utils.Log4j;
+import static org.testng.Assert.assertEquals;
+
+public class TC_JOOMLA_WEBLINKS_004 extends TestHelper {
+
+	HomePage homePage = new HomePage();
+	WebLinksPage webLinksPage = new WebLinksPage();
+	WebLinksNewPage webLinksNewPage = new WebLinksNewPage();
+		
+	String title = "Test WEBLINKS_004"+Utilities.timer_hhmmss();
+	String url = "http://www.joomla.org";
+	
+	public TC_JOOMLA_WEBLINKS_004() {
+
+	}
+	@Test(description = "TC_JOOMLA_WEBLINKS_004 - Verify user can unpublish a published web link")
+	public void f() throws InterruptedException {
+		
+		Log4j.info("Step 1. Login");
+		LoginPage.login(Constants.LOGIN_USERNAME, Constants.LOGIN_PASSWORD);
+				
+		Log4j.info("Step 2. Go to Weblinks page");
+		homePage.goToWebLinksPage();
+		
+		Log4j.info("Step 3. Click button new redirect New WebLinks page");
+		webLinksPage.clickBtnNew();
+		
+		Log4j.info("Step 4. Create new");
+		webLinksNewPage.createNew(title,Utilities.faker_title(),url,Utilities.getContent(),webLinksNewPage.status_Published);
+		
+		Log4j.info("Step 5. save and close");
+		webLinksNewPage.clickBtnSaveAndClose();
+				
+		Log4j.info("Step 6. Verify the web link is saved successfully");
+		assertEquals(webLinksPage.checkSaved(), true);
+		assertEquals(webLinksPage.checkCreationByTitle(title), true);
+
+		Log4j.info("Step 7. Check on the recently added article's checkbox");
+		webLinksPage.selectCheckboxWithTitle(title);
+		
+		Log4j.info("Step 8. Click on 'Publich' icon of the top right toolbar");
+		webLinksPage.click_button(webLinksPage._btnUnpublish);
+				
+		Log4j.info("Step 9. Verify the web link is published successfully");
+		assertEquals(webLinksPage.checkIconUnpublishDisplayed(title), true);
+		assertEquals(webLinksPage.checkUnpublished(), true);
+		
+		Log4j.info("Logout");
+		GeneralPage.logout();
+	}
+}
