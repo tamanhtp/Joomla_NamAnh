6eec439d2e8c2a36f2bb48acc6cc791411f70aa6
tamanhtp <tamanhtp@gmail.com>
2019-12-23 11:21:42 +0700

    Update static, finish weblink002

--- src/test/java/test/pages/BannersPage.java
+++ src/test/java/test/pages/BannersPage.java
@@ -3,28 +3,17 @@ package test.pages;
 import org.openqa.selenium.By;
 
 import test.utils.BrowserHelper;
-import test.utils.Utilities;
 
-public class BannersPage extends GeneralPage{
-	
+public class BannersPage extends GeneralPage {
+
 	private static By leftmenu_Banners = By.xpath("//ul[@id='submenu']//a[contains(text(),'Banners')]");
 	private static By leftmenu_Banners_Categories = By.xpath("//ul[@id='submenu']//a[contains(text(),'Categories')]");
 	private static By leftmenu_Banners_Clients = By.xpath("//ul[@id='submenu']//a[contains(text(),'Clients')]");
 	private static By leftmenu_Banners_Tracks = By.xpath("//ul[@id='submenu']//a[contains(text(),'Tracks')]");
-	private static By txt_name = By.id("jform_name");
-	private static By txt_contact = By.id("jform_contact");
-	
+
 	private static By client_saved = By.xpath("//div[@class='alert-message' and contains(text(),'Client saved.')]");
-	
-	// create new Banners Clients with title, alias, status
-		public static void createNew(String name, String contact, By status) {
-			Utilities.sendkeys(BannersPage.txt_name, name);
-			Utilities.sendkeys(BannersPage.txt_contact, contact);
-			Utilities.click(BannersPage.dropdownlist_Status);
-			Utilities.click(status);
-		}
-	
-	public  void goToBanners() {
+
+	public void goToBanners() {
 		BrowserHelper.driver.findElement(leftmenu_Banners).click();
 	}
 
@@ -43,7 +32,5 @@ public class BannersPage extends GeneralPage{
 	public static boolean checkSavedSuccessfully() {
 		return BrowserHelper.driver.findElement(client_saved).isDisplayed();
 	}
-	
-	
-	
+
 }
--- src/test/java/testNG/TC_JOOMLA_WEBLINKS_002.java
+++ src/test/java/testNG/TC_JOOMLA_WEBLINKS_002.java
@@ -18,22 +18,16 @@ public class TC_JOOMLA_WEBLINKS_002 extends TestHelper {
 	WebLinksPage webLinksPage = new WebLinksPage();
 	WebLinksNewPage webLinksNewPage = new WebLinksNewPage();
 		
-	String title = "Test Weblink 1";
+	String title = "Test Weblink 1"+Utilities.timer_hhmmss();
 	String url = "http://www.joomla.org";
-	String title_new = "Test Weblink 2";
+	String title_new = "Test Weblink 2"+Utilities.timer_hhmmss();
 	String url_new = "http://www.google.com";
 	
 	public TC_JOOMLA_WEBLINKS_002() {
-		Constants.setFirefoxBrowser();
-//		Constants.setChromeBrowser();
-//		Constants.setIEBrowser();
-//		Constants.setEDGEBrowser();
 
 	}
-	@Test(description = "TC_JOOMLA_WEBLINKS_001 - Verify user can create new web link with valid information")
+	@Test(description = "TC_JOOMLA_WEBLINKS_002 - Verify that user can edit a client")
 	public void f() throws InterruptedException {
-		System.out.println(Constants.BROWSER);
-//		String title = Utilities.faker_title();
 		
 		Log4j.info("Step 1. Login");
 		LoginPage.login(Constants.LOGIN_USERNAME, Constants.LOGIN_PASSWORD);
@@ -45,35 +39,35 @@ public class TC_JOOMLA_WEBLINKS_002 extends TestHelper {
 		webLinksPage.clickBtnNew();
 		
 		Log4j.info("Step 4. Create new");
-		webLinksNewPage.createNew(title,Utilities.faker_title(),url,Utilities.faker_content(),GeneralPage.status_Unpublished);
+		webLinksNewPage.createNew(title,Utilities.faker_title(),url,Utilities.faker_content(),webLinksNewPage.status_Unpublished);
 		
 		Log4j.info("Step 5. save and close");
 		webLinksNewPage.clickBtnSaveAndClose();
 				
 		Log4j.info("Step 6. Verify the web link is saved successfully");
-		assertEquals(GeneralPage.checkSaved(), true);
-		assertEquals(GeneralPage.checkCreationByTitle(title), true);
+		assertEquals(webLinksPage.checkSaved(), true);
+		assertEquals(webLinksPage.checkCreationByTitle(title), true);
 
 		Log4j.info("Step 7. Check on the recently added article's checkbox");
 		webLinksPage.selectCheckboxWithTitle(title);
 		
 		Log4j.info("Step 8. Click on 'Edit' icon of the top right toolbar");
-		webLinksPage.click_button(GeneralPage._btnEdit);
+		webLinksPage.click_button(webLinksPage._btnEdit);
 		
 		Log4j.info("Step 9. Enter a new title on 'Title' field");
-		Utilities.clear(GeneralPage._txtTitle);
-		Utilities.sendkeys(GeneralPage._txtTitle, title_new);
+		Utilities.clear(webLinksPage._txtTitle);
+		Utilities.sendkeys(webLinksPage._txtTitle, title_new);
 		
 		Log4j.info("Step 10. Enter new valid URL into 'URL' text field");
-		Utilities.clear(GeneralPage._txtUrl);
-		Utilities.sendkeys(GeneralPage._txtUrl, url_new);
+		Utilities.clear(webLinksPage._txtUrl);
+		Utilities.sendkeys(webLinksPage._txtUrl, url_new);
 		
 		Log4j.info("Step 11. Save");
-		webLinksNewPage.click_button(GeneralPage._btnSaveAndClose);
+		webLinksNewPage.click_button(webLinksPage._btnSaveAndClose);
 		
 		Log4j.info("Step 12. Verify the web link is saved successfully");
-		assertEquals(GeneralPage.checkSaved(), true);
-		assertEquals(GeneralPage.checkCreationByTitle(title), true);
+		assertEquals(webLinksPage.checkSaved(), true);
+		assertEquals(webLinksPage.checkCreationByTitle(title_new), true);
 		
 		Log4j.info("Logout");
 		GeneralPage.logout();
